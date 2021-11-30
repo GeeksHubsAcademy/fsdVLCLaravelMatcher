4bf6e48509cf5eb1c9de11edb9460a59337ea93f
Dave86dev <davidochando1986@gmail.com>
2021-11-30 12:06:15 +0100

    Kickstart proyecto. Primeras migraciones y relaciones creadas

---
+++ app/Models/User.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model
+{
+    use HasFactory;
+
+    //
+    protected $fillable = [
+        'email', 'name', 'gender', 'orientation', 'status', 'intention', 'age', 
+        'surname'
+    ];
+
+    protected $hidden = ['password'];
+
+    public function messages()
+    {
+        return $this->hasMany('App\Models\Message');
+    }
+}
